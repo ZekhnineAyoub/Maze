a4993dc0cd482aa3c84f51bfe7c7b8f237d96771
ZEKHNINE Ayoub <ayoub.zekhnine@efs.sante.fr>
2021-10-20 12:07:45 +0200

    new commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ MAZE = """
 
 START = '.'
 GOAL = '*'
-BORDER = '#'
+WALL = '#'
 
 UP = 'U'
 DOWN = 'D'
@@ -18,8 +18,9 @@ LEFT = 'L'
 RIGHT = 'R'
 ACTIONS = [UP, DOWN, LEFT, RIGHT]
 
+REWARD_OUT = -5
 REWARD_BORDER = -2
-REWARD_GOAL = 1
+REWARD_GOAL = 10
 REWARD_EMPTY = 0
 
 
@@ -63,14 +64,18 @@ class Environment:
             new_state = (state[0], state[1] + 1)
 
         if new_state in self.states:
-            if self.__states[new_state] in [BORDER, START]:
+            if self.__states[new_state] in [WALL, START]:
                 reward = REWARD_BORDER
             elif self.__states[new_state] == GOAL:
                 reward = REWARD_GOAL
             else:
                 reward = REWARD_EMPTY
 
-            agent.update(new_state, reward)
+            state = new_state
+        else:
+            reward = REWARD_OUT
+
+        agent.update(state, reward)
 
 
 class Agent:
@@ -78,10 +83,27 @@ class Agent:
         self.__state = environment.start
         self.__score = 0
         self.__last_action = None
+        self.__qtable = {}
+
+        for s in environment.states:
+            self.__qtable[s] = {}
+
+            for a in ACTIONS:
+                self.__qtable[s][a] = 0.0
 
     def update(self, state: str, reward: int):
         self.__state = state
         self.__score += reward
+
+    def best_action(self):
+        rewards = self.__qtable[self.__state]
+        best = None
+
+        for a in rewards:
+            if best is None or rewards[a] > rewards[best]:
+                best = a
+
+        return best
 
     @property
     def state(self):
@@ -97,6 +119,9 @@ if __name__ == "__main__":
     print(env.states)
 
     agent = Agent(env)
+    action = agent.best_action()
+    print(action)
+
     print(agent.state, agent.score)
-    env.apply(agent, DOWN)
+    env.apply(agent, action)
     print(agent.state, agent.score)
